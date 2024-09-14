220c59cf439e20b18ef99668ad92b2a644d201f2
Andrey6691 <abw66@yandex.ru>
2024-09-14 16:17:34 +0300

    Add files via upload

--- module_2.3.py
+++ module_2.3.py
@@ -1,11 +1,13 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 first = 0
 while first < len(my_list):
-    if my_list[first] > 0:
+    if my_list[first] >0:
         print(my_list[first])
         first += 1
         continue
-    else:
+    if my_list[first] == 0:
         first += 1
         continue
-    print('Я за циклом')
+    else:
+        break
+print('Я за циклом')
